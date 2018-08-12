1cfa45ba0ed866d4a36ecfcb991434db1987c2b3
purulalwani <purulalwani@gmail.com>
2018-08-12 14:07:48 +0530

    added share and verify claim.

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -68,6 +68,36 @@ window.App = {
         approveClaimOwnerOption.text = accounts[i];
         approveClaimOwnerOption.value = accounts[i];
         approveClaimOwnerSelect.options.add(approveClaimOwnerOption, i);
+
+        var shareClaimOwnerSelect = document.getElementById("shareClaimOwner");
+        var shareClaimOwnerOption = document.createElement("option");
+
+        shareClaimOwnerOption.text = accounts[i];
+        shareClaimOwnerOption.value = accounts[i];
+        shareClaimOwnerSelect.options.add(shareClaimOwnerOption, i);
+
+        var shareClaimThirdPartySelect = document.getElementById("shareClaimThirdParty");
+        var shareClaimThirdPartyOption = document.createElement("option");
+
+        shareClaimThirdPartyOption.text = accounts[i];
+        shareClaimThirdPartyOption.value = accounts[i];
+        shareClaimThirdPartySelect.options.add(shareClaimThirdPartyOption, i);
+
+        var verifyClaimOwnerSelect = document.getElementById("verifyClaimOwner");
+        var verifyClaimOwnerOption = document.createElement("option");
+
+        verifyClaimOwnerOption.text = accounts[i];
+        verifyClaimOwnerOption.value = accounts[i];
+        verifyClaimOwnerSelect.options.add(verifyClaimOwnerOption, i);
+
+        var verifyClaimIssuerSelect = document.getElementById("verifyClaimIssuer");
+        var verifyClaimIssuerOption = document.createElement("option");
+
+        verifyClaimIssuerOption.text = accounts[i];
+        verifyClaimIssuerOption.value = accounts[i];
+        verifyClaimIssuerSelect.options.add(verifyClaimIssuerOption, i);
+
+
       }
 
       //self.refreshBalance();
@@ -101,8 +131,10 @@ window.App = {
       return instance.createIdentity(ownerId, ownerType, name, {from: ownerId});
     }).then(function(value) {
       self.setStatus("Identity created.");
+      alert("Identity created.");
     }).catch(function(e) {
       console.log(e);
+      alert("Error creating identity; see log.");
       self.setStatus("Error creating identity; see log.");
     });
    }
@@ -132,9 +164,11 @@ window.App = {
       return instance.createClaim(owner, issuer, web3.fromUtf8(data), claimType, {from: owner, gas: 3000000});
     }).then(function() {
       self.setStatus("Transaction complete!");
+      alert("Claim created.");
       //self.refreshBalance();
     }).catch(function(e) {
       console.log(e);
+      alert("Error creating claim; see log.");
       self.setStatus("Error creating claim; see log.");
     });
   }
@@ -232,8 +266,8 @@ window.App = {
 
 
       var claimDetailsTable = $("#claimDetailsTable");
-      var claimDetailsHtml = "<tr><th>Owner</th><th>Issuer</th><th>Claim Type</th><th>Claim Status</th><th>Claim Data</th><th>Action</th></tr>";
-      claimDetailsHtml = claimDetailsHtml + "<tr><td>" + owner + "</td><td>" + issuer + "</td><td>" + claimType + "</td><td>" + claimStatus + "</td><td>" + claimData + "</td><td>" + action + "</td></tr>";
+      var claimDetailsHtml = "<thead><tr><th>Owner</th><th>Issuer</th><th>Claim Type</th><th>Claim Status</th><th>Claim Data</th><th>Action</th></tr><thead>";
+      claimDetailsHtml = "<tbody>" + claimDetailsHtml + "<tr><td>" + owner + "</td><td>" + issuer + "</td><td>" + claimType + "</td><td>" + claimStatus + "</td><td>" + claimData + "</td><td>" + action + "</td></tr></tbody>";
       claimDetailsTable.html(claimDetailsHtml);
       //self.refreshBalance();
     // }).then(function(result){
@@ -283,10 +317,331 @@ window.App = {
       return identityInstance.approveClaim(approveClaimOwnerClaim, result[0], result[1], result[3], web3.fromUtf8(signature), {from: result[1], gas: 3000000});
     }).then(function() {
       self.setStatus("Transaction complete!");
+      alert("Claim approved!!!")
       //self.refreshBalance();
     }).catch(function(e) {
       console.log(e);
+      alert("Error approving claim; see log.");
       self.setStatus("Error approving claim; see log.");
+    });
+  }
+  ,
+  //
+  getClaimsShare: function() {
+    var self = this;
+
+
+    var shareClaimOwnerSelect = document.getElementById("shareClaimOwner");
+    var shareClaimOwnerSelectedIndex = shareClaimOwnerSelect.selectedIndex;
+    var shareClaimOwner = shareClaimOwnerSelect.options[shareClaimOwnerSelectedIndex].value;
+
+
+    var identityInstance;
+    Identity.deployed().then(function(instance) {
+      identityInstance = instance;
+      return instance.getClaims.call(shareClaimOwner, {from: shareClaimOwner});
+    }).then(function(result) {
+      self.setStatus("Transaction complete!");
+      console.log("Result -> ", result);
+      for(var i=0; i<result.length; i++){
+
+        var shareClaimOwnerClaimSelect = document.getElementById("shareClaimOwnerClaims");
+        var shareClaimOwnerClaimOption = document.createElement("option");
+
+        console.log("Chaim ID ->", result[i]);
+        shareClaimOwnerClaimOption.text = result[i];
+        shareClaimOwnerClaimOption.value = result[i];
+        shareClaimOwnerClaimSelect.options.add(shareClaimOwnerClaimOption, i);
+        //return identityInstance.getClaimById.call(result[i], {from: approveClaimOwner});
+      }
+      //self.refreshBalance();
+    // }).then(function(result){
+    //   console.log("claim result -> ", result);
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error getting claims; see log.");
+    });
+  }
+  ,
+  //
+  getClaimByIdShare: function() {
+    var self = this;
+
+    var shareClaimOwnerSelect = document.getElementById("shareClaimOwner");
+    var shareClaimOwnerSelectedIndex = shareClaimOwnerSelect.selectedIndex;
+    var shareClaimOwner = shareClaimOwnerSelect.options[shareClaimOwnerSelectedIndex].value;
+
+
+    var shareClaimOwnerClaimSelect = document.getElementById("shareClaimOwnerClaims");
+    var shareClaimOwnerClaimSelectedIndex = shareClaimOwnerClaimSelect.selectedIndex;
+    var shareClaimOwnerClaim = shareClaimOwnerClaimSelect.options[shareClaimOwnerClaimSelectedIndex].value;
+
+
+    var identityInstance;
+    Identity.deployed().then(function(instance) {
+      identityInstance = instance;
+      return instance.getClaimById.call(shareClaimOwnerClaim, {from: shareClaimOwner});
+    }).then(function(result) {
+      self.setStatus("Transaction complete!");
+      console.log("Result -> ", result);
+
+      console.log("Data -> ", web3.toUtf8(result[2]));
+      console.log("Cliam Type -> ", result[3].toNumber());
+      console.log("Claim Status -> ", result[4].toNumber());
+
+      var owner = result[0];
+      var issuer = result[1];
+      var claimStatus;
+      var claimType;
+      var action = "";
+      if(result[3].toNumber() == 0){
+        claimType = "Address Verification";
+      }else if (result[3].toNumber() == 1){
+        claimType = "Name Verification";
+      }else if(result[3].toNumber() == 2){
+        claimType = "Email Verification";
+      }
+
+      var shareParams = shareClaimOwnerClaim + "," + result[0] + "," + result[1] + "," + result[2] + "," + result[3];
+      if(result[4].toNumber() == 0){
+        claimStatus = "Created";
+        //action = "<button id=\"getClaimByIdShare\" class=\"btn btn-primary\" onclick=\"App.shareClaim(" + shareParams + ")\">Share</button>";
+      }else if (result[4].toNumber() == 1){
+        claimStatus = "Approved";
+      }else if (result[4].toNumber() == 2){
+        claimStatus = "Rejected";
+      }
+
+
+
+      var claimData = web3.toUtf8(result[2]);
+
+
+
+      var claimDetailsTable = $("#shareClaimDetailsTable");
+      var claimDetailsHtml = "<thead><tr><th>Owner</th><th>Issuer</th><th>Claim Type</th><th>Claim Status</th><th>Claim Data</th></tr></thead>";
+      claimDetailsHtml = "<tbody>" + claimDetailsHtml + "<tr><td>" + owner + "</td><td>" + issuer + "</td><td>" + claimType + "</td><td>" + claimStatus + "</td><td>" + claimData + "</td></tr><tbody>";
+      claimDetailsTable.html(claimDetailsHtml);
+      //self.refreshBalance();
+    // }).then(function(result){
+    //   console.log("claim result -> ", result);
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error getting claim details; see log.");
+    });
+  }
+  ,
+
+  shareClaim: function() {
+
+    //console.log("Input data -> ", claimId, owner, issuer, data, claimType);
+
+    var shareClaimOwnerSelect = document.getElementById("shareClaimOwner");
+    var shareClaimOwnerSelectedIndex = shareClaimOwnerSelect.selectedIndex;
+    var shareClaimOwner = shareClaimOwnerSelect.options[shareClaimOwnerSelectedIndex].value;
+
+    var shareClaimOwnerClaimSelect = document.getElementById("shareClaimOwnerClaims");
+    var shareClaimOwnerClaimSelectedIndex = shareClaimOwnerClaimSelect.selectedIndex;
+    var shareClaimOwnerClaim = shareClaimOwnerClaimSelect.options[shareClaimOwnerClaimSelectedIndex].value;
+
+    var shareClaimThirdPartySelect = document.getElementById("shareClaimThirdParty");
+    var shareClaimThirdPartySelectedIndex = shareClaimThirdPartySelect.selectedIndex;
+    var shareClaimThirdParty = shareClaimThirdPartySelect.options[shareClaimThirdPartySelectedIndex].value;
+
+    //var messageHash = web3.keccak256(owner, issuer, claimType, data);
+
+
+    //var prefixedHash = keccak256("\x19Ethereum Signed Message:\n32", messageHash);
+
+    //var signature = web3.eth.sign(messageHash, issuer);
+
+    //console.log("Signature -> ", signature);
+
+    var signature = "";
+
+
+
+    console.log("Sharing claim...");
+    var self = this;
+
+
+    var identityInstance;
+    Identity.deployed().then(function(instance) {
+      identityInstance = instance;
+
+      //meta = instance;
+      return identityInstance.shareClaim(shareClaimOwnerClaim, shareClaimThirdParty, {from: shareClaimOwner, gas: 3000000});
+    }).then(function() {
+      self.setStatus("Transaction complete!");
+      alert("Claim shared.")
+      //self.refreshBalance();
+    }).catch(function(e) {
+      console.log(e);
+      alert("Error sharing claim; see log.")
+      self.setStatus("Error sharing claim; see log.");
+    });
+  }
+
+  ,
+  // Verify Claim functions
+
+  getClaimsVerify: function() {
+    var self = this;
+
+
+    var verifyClaimOwnerSelect = document.getElementById("verifyClaimOwner");
+    var verifyClaimOwnerSelectedIndex = verifyClaimOwnerSelect.selectedIndex;
+    var verifyClaimOwner = verifyClaimOwnerSelect.options[verifyClaimOwnerSelectedIndex].value;
+
+
+    var identityInstance;
+    Identity.deployed().then(function(instance) {
+      identityInstance = instance;
+      return instance.getClaims.call(verifyClaimOwner, {from: verifyClaimOwner});
+    }).then(function(result) {
+      self.setStatus("Transaction complete!");
+      console.log("Result -> ", result);
+      for(var i=0; i<result.length; i++){
+
+        var verifyClaimOwnerClaimSelect = document.getElementById("verifyClaimOwnerClaims");
+        var verifyClaimOwnerClaimOption = document.createElement("option");
+
+        console.log("Chaim ID ->", result[i]);
+        verifyClaimOwnerClaimOption.text = result[i];
+        verifyClaimOwnerClaimOption.value = result[i];
+        verifyClaimOwnerClaimSelect.options.add(verifyClaimOwnerClaimOption, i);
+        //return identityInstance.getClaimById.call(result[i], {from: approveClaimOwner});
+      }
+      //self.refreshBalance();
+    // }).then(function(result){
+    //   console.log("claim result -> ", result);
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error getting claims; see log.");
+    });
+  }
+  ,
+  //
+  getClaimByIdVerify: function() {
+    var self = this;
+
+    var verifyClaimOwnerSelect = document.getElementById("verifyClaimOwner");
+    var verifyClaimOwnerSelectedIndex = verifyClaimOwnerSelect.selectedIndex;
+    var verifyClaimOwner = verifyClaimOwnerSelect.options[verifyClaimOwnerSelectedIndex].value;
+
+
+    var verifyClaimOwnerClaimSelect = document.getElementById("verifyClaimOwnerClaims");
+    var verifyClaimOwnerClaimSelectedIndex = verifyClaimOwnerClaimSelect.selectedIndex;
+    var verifyClaimOwnerClaim = verifyClaimOwnerClaimSelect.options[verifyClaimOwnerClaimSelectedIndex].value;
+
+
+    var identityInstance;
+    Identity.deployed().then(function(instance) {
+      identityInstance = instance;
+      return instance.getClaimById.call(verifyClaimOwnerClaim, {from: verifyClaimOwner});
+    }).then(function(result) {
+      self.setStatus("Transaction complete!");
+      console.log("Result -> ", result);
+
+      console.log("Data -> ", web3.toUtf8(result[2]));
+      console.log("Cliam Type -> ", result[3].toNumber());
+      console.log("Claim Status -> ", result[4].toNumber());
+
+      var owner = result[0];
+      var issuer = result[1];
+      var claimStatus;
+      var claimType;
+      var action = "";
+      if(result[3].toNumber() == 0){
+        claimType = "Address Verification";
+      }else if (result[3].toNumber() == 1){
+        claimType = "Name Verification";
+      }else if(result[3].toNumber() == 2){
+        claimType = "Email Verification";
+      }
+
+      var shareParams = verifyClaimOwnerClaim + "," + result[0] + "," + result[1] + "," + result[2] + "," + result[3];
+      if(result[4].toNumber() == 0){
+        claimStatus = "Created";
+        //action = "<button id=\"getClaimByIdShare\" class=\"btn btn-primary\" onclick=\"App.shareClaim(" + shareParams + ")\">Share</button>";
+      }else if (result[4].toNumber() == 1){
+        claimStatus = "Approved";
+      }else if (result[4].toNumber() == 2){
+        claimStatus = "Rejected";
+      }
+
+
+
+      var claimData = web3.toUtf8(result[2]);
+
+
+
+      var claimDetailsTable = $("#verifyClaimDetailsTable");
+      var claimDetailsHtml = "<thead><tr><th>Owner</th><th>Issuer</th><th>Claim Type</th><th>Claim Status</th><th>Claim Data</th></tr></thead>";
+      claimDetailsHtml = "<tbody>" + claimDetailsHtml + "<tr><td>" + owner + "</td><td>" + issuer + "</td><td>" + claimType + "</td><td>" + claimStatus + "</td><td>" + claimData + "</td></tr><tbody>";
+      claimDetailsTable.html(claimDetailsHtml);
+      //self.refreshBalance();
+    // }).then(function(result){
+    //   console.log("claim result -> ", result);
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error getting claim details; see log.");
+    });
+  }
+  ,
+
+  verifyClaim: function() {
+
+    //console.log("Input data -> ", claimId, owner, issuer, data, claimType);
+
+    var verifyClaimOwnerSelect = document.getElementById("verifyClaimOwner");
+    var verifyClaimOwnerSelectedIndex = verifyClaimOwnerSelect.selectedIndex;
+    var verifyClaimOwner = verifyClaimOwnerSelect.options[verifyClaimOwnerSelectedIndex].value;
+
+    var verifyClaimOwnerClaimSelect = document.getElementById("verifyClaimOwnerClaims");
+    var verifyClaimOwnerClaimSelectedIndex = verifyClaimOwnerClaimSelect.selectedIndex;
+    var verifyClaimOwnerClaim = verifyClaimOwnerClaimSelect.options[verifyClaimOwnerClaimSelectedIndex].value;
+
+    var verifyClaimIssuerSelect = document.getElementById("verifyClaimIssuer");
+    var veirfyClaimIssuerSelectedIndex = verifyClaimIssuerSelect.selectedIndex;
+    var verifyClaimIssuer = verifyClaimIssuerSelect.options[veirfyClaimIssuerSelectedIndex].value;
+
+    //var messageHash = web3.keccak256(owner, issuer, claimType, data);
+
+
+    //var prefixedHash = keccak256("\x19Ethereum Signed Message:\n32", messageHash);
+
+    //var signature = web3.eth.sign(messageHash, issuer);
+
+    //console.log("Signature -> ", signature);
+
+    var signature = "";
+
+
+
+    console.log("verifying claim...");
+    var self = this;
+
+
+    var identityInstance;
+    Identity.deployed().then(function(instance) {
+      identityInstance = instance;
+
+      //meta = instance;
+      return identityInstance.verifyClaim(verifyClaimOwnerClaim, verifyClaimIssuer, {from: verifyClaimOwner, gas: 3000000});
+    }).then(function(result) {
+      self.setStatus("Transaction complete!");
+      console.log(result);
+      if(result == true){
+      alert("Claim verified.")
+    } else {
+      alert("Claim not verified.")
+    }
+      //self.refreshBalance();
+    }).catch(function(e) {
+      console.log(e);
+      alert("Error verifying claim; see log.")
+      self.setStatus("Error verifying claim; see log.");
     });
   }
 
